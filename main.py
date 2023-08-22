886750251b7def01e335d5cf4bdc359e8f17b744
Alberto Verna <alberto.verna@hotmail.com>
2023-08-22 15:54:01 +0200

    Rimuovi .env, Percorsi relativi

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import logging
 logging.warning('Watch out!') 
 logging.getLogger().setLevel(logging.DEBUG)
 
-#grafo = onnx_rust.leggi_file("/home/sordello/Desktop/Group27/onnxFile/mnist-12/mnist-12.onnx")
+#grafo = onnx_rust.leggi_file("/home/alberto/Dropbox (Politecnico Di Torino Studenti)/Programmazione di Sistema/onnx-rust/onnxFile/mnist-12/mnist-12.onnx")
 #print(grafo)
 
 
-x= onnx_rust.interferenza("/home/sordello/Desktop/Group27/onnxFile/mnist-12/mnist-12.onnx",
-                   "/home/sordello/Desktop/Group27/onnxFile/mnist-12/test_data_set_0/input_0.pb")
-expected_output = onnx_rust.leggi_file_dati("/home/sordello/Desktop/Group27/onnxFile/mnist-12/test_data_set_0/output_0.pb")
+x= onnx_rust.interferenza(
+    "onnxFile/mnist-12/mnist-12.onnx",
+    "onnxFile/mnist-12/test_data_set_0/input_0.pb"
+)
+expected_output = onnx_rust.leggi_file_dati("onnxFile/mnist-12/test_data_set_0/output_0.pb")
 
 print("Result of interference :",x)
 print("Expected output :",expected_output)
