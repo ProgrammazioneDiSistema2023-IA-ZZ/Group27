0cfeac4d047f7284e35774ad2f0c2cc5d7076170
Alberto Verna <alberto.verna@hotmail.com>
2023-08-24 19:40:25 +0200

    Quick fix test.py

--- python/test.py
+++ python/test.py
@@ -7,10 +7,10 @@ logging.getLogger().setLevel(logging.DEBUG)
 #print(grafo)
 
 x= onnx_rust.inference(
-    "../onnxFile/googlenet-12/googlenet-12.onnx",
-    "../onnxFile/googlenet-12/test_data_set_0/input_0.pb"
+    "./onnxFile/googlenet-12/googlenet-12.onnx",
+    "./onnxFile/googlenet-12/test_data_set_0/input_0.pb"
 )
-expected_output = onnx_rust.read_data_file("onnxFile/googlenet-12/test_data_set_0/output_0.pb")
+expected_output = onnx_rust.read_data_file("./onnxFile/googlenet-12/test_data_set_0/output_0.pb")
 
 print("Result of inference :",x)
 print("Expected output :",expected_output)
